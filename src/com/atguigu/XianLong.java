cf836d0cb4725a0d02b51c3ec7dbdf58278a79e3
qiusai <qiuxiaomeng@sina.cn>
2016-12-29 09:42:36 +0800

    create xianlong

---
+++ src/com/atguigu/XianLong.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.atguigu;
+
+/**
+ * @author sai
+ * 类名：
+ * 创建日期：
+ * 版本：
+ */
+public class XianLong {
+
+	/**
+	 * 方法名
+	 *功能描述
+	 *作者
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
